49d64d88eb0517ecdcdd58c233738c362cd45694
Prathamesh <parthshelar0077@gmail.com>
2026-09-08 11:36:48 +0530

    Refactor tests to use Assignment class

--- test_assignment.py
+++ test_assignment.py
@@ -1,12 +1,26 @@
-from assignment import create_assignment
+from assignment import Assignment
 
 
-def test_create_assignment():
-    assignment = create_assignment(
-        "Complete DevOps Practical",
-        "Software Engineering"
+def test_assignment_creation():
+    assignment = Assignment(
+        "DevOps Practical",
+        "Software Engineering",
+        "10 September 2026"
     )
 
-    assert assignment["title"] == "Complete DevOps Practical"
-    assert assignment["subject"] == "Software Engineering"
-    assert assignment["status"] == "Pending"
+    assert assignment.title == "DevOps Practical"
+    assert assignment.subject == "Software Engineering"
+    assert assignment.deadline == "10 September 2026"
+    assert assignment.status == "Pending"
+
+
+def test_complete_assignment():
+    assignment = Assignment(
+        "DevOps Practical",
+        "Software Engineering",
+        "10 September 2026"
+    )
+
+    assignment.complete_assignment()
+
+    assert assignment.status == "Completed"
